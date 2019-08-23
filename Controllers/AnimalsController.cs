de22d501d639d5f5d321de99370a96acbe8c9416
HoldenJC <holdenj_c@yahoo.com>
2019-08-23 12:15:43 -0700

    begin adding versioning compatibility to API

--- Controllers/AnimalsController.cs
+++ Controllers/AnimalsController.cs
@@ -7,9 +7,58 @@ using Shelter.Models;
 
 namespace Shelter.Controllers
 {
+  [ApiVersion("1.0")]
   [Route("api/[controller]")]
   [ApiController]
   public class AnimalsController : ControllerBase
+  {
+    private ShelterContext _db = new ShelterContext();
+
+    // GET api/animals
+    [HttpGet]
+    public ActionResult<IEnumerable<Animal>> Get()
+    {
+      return _db.Animals.ToList();
+    }
+
+    // GET api/animals/5
+    [HttpGet("{id}")]
+    public ActionResult<Animal> Get(int id)
+    {
+      return _db.Animals.FirstOrDefault(x => x.AnimalId == id);
+    }
+
+    // POST api/values
+    [HttpPost]
+    public void Post([FromBody] Animal animal)
+    {
+      _db.Animals.Add(animal);
+      _db.SaveChanges();
+    }
+
+    // PUT api/animals/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Animal animal)
+    {
+      animal.AnimalId = id;
+      _db.Entry(animal).State = EntityState.Modified;
+      _db.SaveChanges();
+    }
+
+    // DELETE api/animals/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      var animalToDelete = _db.Animals.FirstOrDefault(x => x.AnimalId == id);
+      _db.Animals.Remove(animalToDelete);
+      _db.SaveChanges();
+    }
+  }
+
+  [ApiVersion("2.0")]
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AnimalsControllerV2 : ControllerBase
   {
     private ShelterContext _db = new ShelterContext();
 
